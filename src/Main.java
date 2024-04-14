6e5b927ce3c6e463d86cba0f045753c4cc568879
tuyen9915 <hat453430@gmail.com>
2024-04-14 21:10:19 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Student student1 = new Student("Nam",23, "HN");
+        Student student2 = new Student("Hung",21, "HN");
+        Student student3 = new Student("Ha",22, "HN");
+        // write your code here
+        Map<Integer, Student> studentMap = new HashMap<Integer, Student>();
+        studentMap.put(1,student1);
+        studentMap.put(2,student2);
+        studentMap.put(3,student3);
+        studentMap.put(4,student1);
+
+        for(Map.Entry<Integer, Student> student : studentMap.entrySet()){
+            System.out.println(student.toString());
+        }
+        System.out.println("...........Set");
+        //  không add trùng, đọc phần tử add sau truớc
+        Set<Student> students = new HashSet<Student>();
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        students.add(student1);
+
+        for(Student student : students){
+            System.out.println(student.toString());
+        }
+    }
+
+}
